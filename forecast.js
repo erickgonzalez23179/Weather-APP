4609cebac5161a9eb224f826988363435fb760cd
Erick Gonzalez <23179@columbushs.com>
2021-03-03 12:59:13 -0500

    finished

--- forecast.js
+++ forecast.js
@@ -1,5 +1,21 @@
 const key = `PtHN4Waw2pq2NyLvIasGf0QmH948LhX3`;
 
+//get weather info
+const getWeather = async (id) => {
+  
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0]
+
+}
+
+
+
+//get city info
 const getCity = async(city) => {
 
   const base = `http://dataservice.accuweather.com/locations/v1/cities/search`;
@@ -12,6 +28,9 @@ const getCity = async(city) => {
 
 };
 
-getCity(`manchester`)
-  .then(data => console.log(data))
-  .catch (err => console.log(err));
+getCity(`manchester`).then(data => {
+  return getWeather(data.Key)
+}).then(data => {
+  console.log(data);
+}).catch (err => console.log(err));
+
