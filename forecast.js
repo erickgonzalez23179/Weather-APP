3eead699c35379e69ac34d12cfc4eee7cb60fc52
Erick Gonzalez <23179@columbushs.com>
2021-03-03 12:39:46 -0500

    finished

--- forecast.js
+++ forecast.js
@@ -1,1 +1,17 @@
 const key = `PtHN4Waw2pq2NyLvIasGf0QmH948LhX3`;
+
+const getCity = async(city) => {
+
+  const base = `http://dataservice.accuweather.com/locations/v1/cities/search`;
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return (data[0]);
+
+};
+
+getCity(`manchester`)
+  .then(data => console.log(data))
+  .catch (err => console.log(err));
