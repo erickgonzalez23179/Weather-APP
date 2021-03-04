1c3805bd388c646ca58c706f17a3b5a12b92153b
Erick <egonzalez23179@columbushs.com>
2021-03-04 18:24:56 -0500

    done

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const cityForm = document.querySelector(`form`);
 const card = document.querySelector(`.card`)
 const details = document.querySelector(`.details`)
+const time = document.querySelector(`img.time`)
+const icon = document.querySelector(`.icon img`)
 
 const updateUI =  (data) =>{
 
@@ -18,11 +20,26 @@ const updateUI =  (data) =>{
 
   `
 
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`
+icon.setAttribute(`src`, iconSrc)
+
+let timeSrc = null;
+if(weather.IsDayTime){
+  timeSrc = `img/day.svg`;
+} else{
+  timeSrc = `img/night.svg`
+}
+
+time.setAttribute(`src`, timeSrc)
+
+
   // remove d-none
   if(card.classList.contains(`d-none`)){
     card.classList.remove(`d-none`)
   }
 }
+
+
 const updateCity = async (city) => {
 
   const cityDets = await getCity(city);
