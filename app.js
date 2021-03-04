262a47ddbe6160e4c10c0c57c136e27cf4ef56a8
Erick <egonzalez23179@columbushs.com>
2021-03-04 18:06:04 -0500

    finished

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const cityForm = document.querySelector(`form`);
+const card = document.querySelector(`.card`)
+const details = document.querySelector(`.details`)
 
+const updateUI =  (data) =>{
+  const cityDets = data.cityDets;
+  const weather = data.weather;
+
+  details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Imperial.Value}</span>
+     <span>&deg;F</span>
+    </div>
+
+  `
+
+  // remove d-none
+  if(card.classList.contains(`d-none`)){
+    card.classList.remove(`d-none`)
+  }
+}
 const updateCity = async (city) => {
 
   const cityDets = await getCity(city);
@@ -7,7 +28,7 @@ const updateCity = async (city) => {
 
   return { cityDets, weather };
 }
-
+  
 cityForm.addEventListener(`submit`, e => {
   //prevent default action
   e.preventDefault();
@@ -17,6 +38,6 @@ cityForm.addEventListener(`submit`, e => {
 
   //update the ui with new city
   updateCity(city)
-  .then(data => console.log(data))
+  .then(data => updateUI(data))
   .catch(err => console.log(err));
 })
